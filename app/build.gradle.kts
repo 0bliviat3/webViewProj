f0d38cca464bb3c0b9cfbd6b569ae13ae585bb97
jaewan <sjo9810@naver.com>
2025-10-30 15:04:25 +0900

    version: 앱 배포를 위한 버전 처리
    - v2 -> v3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.devtool.webview"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
